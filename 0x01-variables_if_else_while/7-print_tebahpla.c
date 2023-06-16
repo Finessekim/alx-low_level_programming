431f2f012816457ddd73a41c9ac864448d43fe14
Brian Kimeli <Brayokimeli@gmail.com>
2023-06-16 03:52:35 -0700

    Alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - prints alphabets in reverse
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; c--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
