7c80d6b29e116fc0abfbb06aa0cc3e943aeb8c78
Brian Kimeli <Brayokimeli@gmail.com>
2023-06-20 09:23:01 -0700

    a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 /**
  * add - adds the integers
- * @a - int to be added to b
- * @b - int to be added to a
+ * @a: int to be added to b
+ * @b: int to be added to a
  * Return: sum of a and b
  */
 int add(int a, int b)
