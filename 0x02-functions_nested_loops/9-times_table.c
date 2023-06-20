0f4db7f005f35c0dcd610c70207e31dfb1919be3
Brian Kimeli <Brayokimeli@gmail.com>
2023-06-20 09:08:25 -0700

    Timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,17 +7,15 @@ void times_table(void)
 {
 	int i, j, k;
 
-
 	for (i = 0; i < 10; i++)
 	{
-	for (j = 0; j < 10; j++)
-	{
+		for (j = 0; j < 10; j++)
+		{
 			k = j * i;
 			if (j == 0)
 			{
 				_putchar(k + '0');
 			}
-
 
 			if (k < 10 && j != 0)
 			{
@@ -32,7 +30,8 @@ void times_table(void)
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
-		}	_putchar('\n');
+		}
+		_putchar('\n');
 	}
 }
 
