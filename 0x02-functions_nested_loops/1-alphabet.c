f259a76a632d31d0e03e4371eafedde1785765bc
Brian Kimeli <Brayokimeli@gmail.com>
2023-06-20 07:13:30 -0700

    Alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet - prints all alphabets in lowercase
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	putchar('\n');
+}
